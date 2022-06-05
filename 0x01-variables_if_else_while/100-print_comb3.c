d46947a99761a06232464834e9daaaabd7ba9df1
Ianwakoli <ianwakoli991@gmail.com>
2022-06-05 10:27:55 +0000

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,18 +11,18 @@ int main(void)
 	for (n = 48; n <= 56; n++)
 	{
 		for (m = 49; m <= 57; m++)
-	{
-	if (m > n)
-	{
-		putchar(n);
-		putchar(m);
-			if (n != 56 || m != 57)
+		{
+			if (m > n)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-	}
-	}
+		}
 	}
 	putchar('\n');
 	return (0);
