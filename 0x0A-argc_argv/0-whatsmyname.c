fb362d340d3f1718fc6354acc4097fda97f7e71c
Ianwakoli <ianwakoli991@gmail.com>
2022-06-28 12:28:30 +0000

    prins program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * prints the name of the program - argv[0]
- * argc; no of arguments passed to the function
- * argv; array of arguments
+ * main - prints the name of the program - argv[0]
+ * @argc: no of arguments passed to the function
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
  */
 
 int main(int argc __attribute__ ((unused)), char **argv)
