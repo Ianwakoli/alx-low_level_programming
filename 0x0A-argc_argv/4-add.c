b4e67698db93deb3eaced627f3c9ba2db11f1ef2
Ianwakoli <ianwakoli991@gmail.com>
2022-06-28 20:16:29 +0000

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,9 +3,10 @@
 #include <ctype.h>
 
 /**
- *main - adds numbers
- *argc: number of arguments passed to the function
- *argv: argument vector of pointers to strings
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
  *Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
