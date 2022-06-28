f7422b5c987802f2d0ecb701d65c2a4e19f282f3
Ianwakoli <ianwakoli991@gmail.com>
2022-06-28 19:39:50 +0000

    prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 /**
- *prints all arguments it recieves
- *argc: number of arguments passed to the function
- *argv: argument vector of pointers to strings
+ * main - prints all arguments it recieves
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
  *Return: always 0
  */
 int main(int argc, char *argv[])
